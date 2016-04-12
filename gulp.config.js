f913ddc8aea8a6bef5518660398dae82d6e5c1ab
vu <vik.ugrin@gmail.com>
2016-04-12 23:07:31 +0300

    update packages

--- gulp.config.js
+++ gulp.config.js
@@ -21,7 +21,8 @@ module.exports = function() {
                 'node_modules/angular2/bundles/angular2-polyfills.js',
                 'node_modules/systemjs/dist/system.src.js',
                 'node_modules/rxjs/bundles/Rx.js',
-                'node_modules/angular2/bundles/angular2.dev.js'
+                'node_modules/angular2/bundles/angular2.dev.js',
+                'node_modules/angular2/bundles/router.dev.js'
             ]
         },
         dev: {
