9d8abb33ed06480fb7ddc4be0ad0a44ff8684b76
vu <vik.ugrin@gmail.com>
2016-04-30 02:21:31 +0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import {Component} from 'angular2/core';
+import {RouteConfig} from 'angular2/router';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.html'
+    templateUrl: 'app/app.html',
+    directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+    {path:'/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true}
+])
 export class AppComponent { }
