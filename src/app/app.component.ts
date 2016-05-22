d60dd128c8670b4ee100f1cf976ebc9c71d2d17a
vu <vik.ugrin@gmail.com>
2016-05-22 11:24:49 +0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,18 @@ import {ROUTER_DIRECTIVES, RouterLink, RouteConfig} from '@angular/router-deprec
 import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {SignInComponent} from './components/account/sign-in/sign-in.component';
 import {SignUpComponent} from './components/account/sign-up/sign-up.component';
-import {MdToolbar} from '@angular2-material/toolbar'
+import {HTTP_PROVIDERS} from '@angular/http'
+import {MdButton} from '@angular2-material/button';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.html',
-    directives: [ROUTER_DIRECTIVES, RouterLink, MdToolbar]
+    directives: [ROUTER_DIRECTIVES, RouterLink, MdButton, MdToolbar, MdIcon, MD_SIDENAV_DIRECTIVES, MD_LIST_DIRECTIVES],
+    providers: [MdIconRegistry, HTTP_PROVIDERS]
 })
 @RouteConfig([
     {path:'/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true},
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
+import {MdButton} from '@angular2-material/button'
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card'
 
 @Component({
     selector: 'dashboard',
     templateUrl: 'app/components/dashboard/dashboard.html',
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, MdButton, MD_CARD_DIRECTIVES]
 })
 
 export class DashboardComponent {
