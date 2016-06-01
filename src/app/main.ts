9717fb92b10af2b3f5dd5e8e66d458a4d62d6297
Viktor Ugrin <vu@norse.digital>
2016-06-01 08:47:48 +0300

    updates

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,7 +4,11 @@ import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import {AppComponent} from './app.component';
 
+import {AuthService} from 'app/auth/auth.service';
+
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
-    provide(LocationStrategy, {useClass : HashLocationStrategy})
+    provide(LocationStrategy, {useClass : HashLocationStrategy}),
+    
+    AuthService
 ]);
