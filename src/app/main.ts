c50cd95661373616a41c19e3d0795eb94395718e
vik-13 <vik.ugrin@gmail.com>
2016-06-07 17:40:56 +0300

    updates

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,6 +3,7 @@ import {provide} from '@angular/core';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {ROUTER_PROVIDERS} from '@angular/router';
 import {AppComponent} from './app.component';
+import {FIREBASE_PROVIDERS, defaultFirebase} from 'angularfire2';
 
 import {AuthService} from 'app/auth/auth.service';
 
@@ -10,5 +11,8 @@ bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     provide(LocationStrategy, {useClass : HashLocationStrategy}),
     
-    AuthService
+    AuthService,
+
+    FIREBASE_PROVIDERS,
+    defaultFirebase('https://torrid-heat-2819.firebaseio.com')
 ]);
--- systemjs.config.js
+++ systemjs.config.js
@@ -1,21 +1,29 @@
-/**
- * System configuration for Angular 2 samples
- * Adjust as necessary for your application needs.
- */
 (function(global) {
-    // map tells the System loader where to look for things
     var map = {
-        'app':                        'dev/app', // 'dist',
-        '@angular':                   'node_modules/@angular',
-        '@angular2-material':          'node_modules/@angular2-material',
+        'app': 'dev/app',
+        '@angular': 'node_modules/@angular',
+        '@angular2-material': 'node_modules/@angular2-material',
+        'firebase': 'node_modules/firebase/lib/firebase-web.js',
+        'angularfire2': 'node_modules/angularfire2',
         'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-        'rxjs':                       'node_modules/rxjs'
+        'rxjs': 'node_modules/rxjs'
     };
-    // packages tells the System loader how to load when no filename and/or no extension
+
     var packages = {
-        'app':                        { main: 'main.js',  defaultExtension: 'js' },
-        'rxjs':                       { defaultExtension: 'js' },
-        'angular2-in-memory-web-api': { defaultExtension: 'js' }
+        'app': {
+            main: 'main.js',
+            defaultExtension: 'js'
+        },
+        'rxjs': {
+            defaultExtension: 'js'
+        },
+        'angular2-in-memory-web-api': {
+            defaultExtension: 'js'
+        },
+        'angularfire2': {
+            defaultExtension: 'js',
+            main: 'angularfire2.js'
+        }
     };
     var ngPackageNames = [
         'common',
@@ -43,13 +51,19 @@
         'toolbar'
     ];
 
-    // Add package entries for angular packages
     ngPackageNames.forEach(function(pkgName) {
-        packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
+        packages['@angular/' + pkgName] = {
+            main: pkgName + '.umd.js',
+            defaultExtension: 'js'
+        };
     });
-    // Add package entries for angular packages
+
     materialPackageNames.forEach(function(pkgName) {
-        packages['@angular2-material/'+pkgName] = { format: 'cjs', main: pkgName + '.js', defaultExtension: 'js' };
+        packages['@angular2-material/' + pkgName] = {
+            format: 'cjs',
+            main: pkgName + '.js',
+            defaultExtension: 'js'
+        };
     });
     var config = {
         map: map,
